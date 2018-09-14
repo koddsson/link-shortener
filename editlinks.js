074a29a234e0390103caf0a15b03e0af67b2f50d
Keith Cirkel <keithamus@github.com>
2018-09-14 01:11:13 +0100

    fix(editlinks): use request.body for plain text bodies

--- editlinks.js
+++ editlinks.js
@@ -11,7 +11,7 @@ app.use(bodyParser({ enableTypes: ['json', 'form', 'text']}))
 
 app.use(
   post('/:id?', async (ctx, id) => {
-    const url = ctx.request.body.url
+    const url = ctx.is('text') ? ctx.request.body : ctx.request.body.url
     ctx.assert(url, 400)
 
     debug(`asked to create link ${id} -> ${url}`)
