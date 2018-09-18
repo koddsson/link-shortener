6cfbea666320d562a638a127b17a7e22929ca7da
Keith Cirkel <keithamus@github.com>
2018-09-18 23:37:32 +0100

    refactor: drop postgres models

--- models.js
+++ models.js
@@ -1,5 +1,4 @@
 import { URL } from 'url'
-import { LinksTable as PgLinksTable, StatsTable as PgStatsTable } from './models.postgres'
 import { LinksTable as SQLinksTable, StatsTable as SQStatsTable } from './models.sqlite'
 import { LinksTable as MemLinksTable, StatsTable as MemStatsTable } from './models.memory'
 import createDebug from 'debug'
@@ -11,7 +10,6 @@ dotenv.config()
 export class LinksTable {
   constructor(url) {
     url = new URL(url)
-    if (url.protocol === 'postgresql:') return new PgLinksTable(url.toString())
     if (url.protocol === 'sqlite:') return new SQLinksTable(url.toString())
     if (url.protocol === 'memory:') return new MemLinksTable(url.toString())
     throw new Error(`unknown database protocol ${url.protocol}`)
@@ -21,7 +19,6 @@ export class LinksTable {
 export class StatsTable {
   constructor(url) {
     url = new URL(url)
-    if (url.protocol === 'postgresql:') return new PgStatsTable(url.toString())
     if (url.protocol === 'sqlite:') return new SQStatsTable(url.toString())
     if (url.protocol === 'memory:') return new MemStatsTable(url.toString())
     throw new Error(`unknown database protocol ${url.protocol}`)
