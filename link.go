f87863e43bbdec1689ed3ef014953d74981d7336
Jason Etcovitch <jasonetco@github.com>
2018-12-11 10:36:08 -0500

    Add timestamp field

--- link.go
+++ link.go
@@ -1,10 +1,24 @@
 package main
 
+import (
+	"time"
+)
+
 type Link struct {
-	ID  string `json:"id" form:"id"`
-	URL string `json:"url" form:"url,omitempty"`
+	ID        string `json:"id" form:"id"`
+	URL       string `json:"url" form:"url,omitempty"`
+	Timestamp time.Time
 }
 
 func (l *Link) String() string {
 	return l.URL
+}
+
+// NewLink creates a new link with a timestamp of the current time
+func NewLink(URL string, ID string) *Link {
+	return &Link{
+		URL:       URL,
+		ID:        ID,
+		Timestamp: time.Now(),
+	}
 }
